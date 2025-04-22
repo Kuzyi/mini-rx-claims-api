d842a1fa18e8368545560f1ad32a82cd6e9fef15
aleck <aleckuzma@gmail.com>
2025-04-22 09:10:00 -0400

    feat(api): scaffold ASP.NET Core project with Claim model and controller

---
+++ mini-rx-claims-api/Controllers/ClaimController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniRxClaimsApi.Models;
+
+namespace MiniRxClaimsApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClaimController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllClaims()
+        {
+            return Ok(new[]
+            {
+                new Claim { Id = 1, MemberId = "ABC123", DrugCode = "RX001", DateFilled = DateTime.UtcNow }
+            });
+        }
+    }
+}
---
+++ mini-rx-claims-api/Models/Claim.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniRxClaimsApi.Models
+{
+    public class Claim
+    {
+        public int Id { get; set; }
+        public string MemberId { get; set; }
+        public string DrugCode { get; set; }
+        public DateTime DateFilled { get; set; }
+    }
+}
